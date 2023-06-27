ba9ad1f0bb1812f5d9251f29ececb07f2f04e95e
Horta <nastyadsc@gmail.com>
2023-06-27 19:15:59 +0200

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,223 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Collections;
+using System.Windows;
+using System;
+using System.Collections.Generic;
+
+//Миронова Анастасия 22-ИСП-2/1
+
+namespace lab6; 
+
+public partial class MainWindow
+{
+    private readonly ArrayList _array = new();
+    private readonly List<double> _list = new();
+    private readonly Random _random = new();
+
+    private readonly Stack.Stack<double> _intermediateStack = new();
+
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        OutputArrayList.ItemsSource = _array;
+    }
+
+    private void TextBox_PreviewKeyDown(object sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Enter)
+        {
+            PushToArray(sender, e);
+        }
+    }
+
+
+    private void ShowCount()
+    {
+        var arrayCopy = (double[])_array.ToArray(typeof(double));
+
+        int count = 0;
+        foreach (var number in arrayCopy)
+        {
+            if (number > 0) count++;
+        }
+
+
+        Count.Text = $"В записанном массиве {count} положительных чисел";
+    }
+
+    private void UpdateStack()
+    {
+        OutputStack.Items.Clear();
+        Stack.Stack<double> outputStack = new();
+
+        foreach (var item in _intermediateStack)
+            outputStack.Enqueue(item);
+        foreach (var item in outputStack)
+        {
+            ListBoxItem listItem = new ListBoxItem { Content = item };
+            OutputStack.Items.Add(listItem);
+        }
+    }
+
+    private void PushToArray(object sender, RoutedEventArgs e)
+    {
+        if (string.IsNullOrEmpty(TextBoxInput.Text))
+        {
+            SetError("Необходимо ввести число");
+        }
+        else
+        {
+            var inputStringArray = TextBoxInput.Text.Split(";");
+
+            if (inputStringArray.Length != 12)
+            {
+                SetError("Необходимо ввести 12 вещественных чисел");
+                return;
+            }
+
+            foreach (var inputString in inputStringArray)
+            {
+                var success = double.TryParse(inputString, out var value);
+
+                if (!success)
+                {
+                    SetError("Данные некорректны");
+                    return;
+                }
+
+                if (Math.Abs(value) <= 10)
+                    _array.Add(value);
+            }
+
+            TextBoxInput.Clear();
+            DisableArrayAddAndFillButton();
+
+            OutputArrayList.Items.Refresh();
+            ShowCount();   
+        }
+    }
+
+    private void NumberInputGotFocus(object sender, RoutedEventArgs e)
+    {
+        ErrorOut.Text = "";
+        TextBoxInput.BorderBrush = Brushes.Black;
+        ErrorOut.Margin = new Thickness();
+        ErrorOut.Padding = new Thickness();
+        ErrorOut.Visibility = Visibility.Hidden;
+    }
+
+    private void SetError(string message)
+    {
+        ErrorOut.Text = message;
+        TextBoxInput.BorderBrush = Brushes.DarkRed;
+        ErrorOut.Margin = new Thickness(0, 0, 0, 10);
+        ErrorOut.Padding = new Thickness(5);
+        ErrorOut.Visibility = Visibility.Visible;
+    }
+
+    private void PushToStack(object sender, RoutedEventArgs e)
+    {
+        if (string.IsNullOrEmpty(TextBoxStackInput.Text))
+        {
+            TextBoxStackInput.BorderBrush = Brushes.DarkRed;
+            TextBoxStackInput.Clear();
+            return;
+        }
+
+        if (!double.TryParse(TextBoxStackInput.Text, out var number))
+        {
+            TextBoxStackInput.BorderBrush = Brushes.DarkRed;
+            TextBoxStackInput.Clear();
+            return;
+        }
+
+        _intermediateStack.Enqueue(number);
+        UpdateStack();
+        TextBoxStackInput.Clear();
+    }
+
+    private void PopToStack(object sender, RoutedEventArgs e)
+    {
+        _intermediateStack.Dequeue();
+        UpdateStack();
+    }
+
+    private void InsertToList(object sender, RoutedEventArgs e)
+    {
+        if (string.IsNullOrEmpty(TextBoxListInput.Text))
+        {
+            TextBoxListInput.BorderBrush = Brushes.DarkRed;
+            TextBoxListInput.Clear();
+            return;
+        }
+
+        if (!double.TryParse(TextBoxListInput.Text, out var number))
+        {
+            TextBoxListInput.BorderBrush = Brushes.DarkRed;
+            TextBoxListInput.Clear();
+            return;
+        }
+
+        _list.Add(number);
+        UpdateListTextBox();
+        TextBoxListInput.Clear();
+        UpdateListSum();
+    }
+
+    private void UpdateListSum()
+    {
+        double sum = 0;
+        foreach (var number in _list)
+        {
+            if (number >= 15)
+                sum += number;
+        }
+
+        ListSum.Text = $"Сумма чисел больше или равных 15: {sum}";
+    }
+
+    private void UpdateListTextBox()
+    {
+        OutputList.Items.Clear();
+        foreach (var item in _list)
+        {
+            var listItem = new ListBoxItem
+            {
+                Content = item
+            };
+
+            OutputList.Items.Add(listItem);
+        }
+    }
+
+    private void FillArray(object sender, RoutedEventArgs e)
+    {
+        for (var _ = 0; _ < 12; _++)
+        {
+            var number = Math.Round(_random.NextDouble() * 10, 3);
+
+            if (Math.Abs(number) <= 10)
+            {
+                _array.Add(_random.Next(2) == 1 ? number : -number);
+            }
+        }
+
+        DisableArrayAddAndFillButton();
+
+        ShowCount();
+        OutputArrayList.Items.Refresh();
+    }
+
+    private void DisableArrayAddAndFillButton()
+    {
+        const string toolTip = "Массив уже был введен";
+        FillArrayButton.IsEnabled = false;
+        FillArrayButton.ToolTip = toolTip;
+
+        AddButton.IsEnabled = false;
+        AddButton.ToolTip = toolTip;
+    }
+}
---
+++ Stack/Stack.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+
+namespace lab6.Stack
+{
+
+    public class Stack<T>:IEnumerable<T>
+    {
+        private Node<T>? _head;
+        private Node<T>? _tail;
+
+        public void Enqueue(T value)
+        {
+            var node = new Node<T>(value);
+            if (_head == null)
+            {
+                _head = node;
+            } else
+            {
+                node.Previous = _tail;
+            }
+            _tail = node;
+        }
+
+        public T? Dequeue()
+        {
+            if (_tail == null)
+                return default;
+
+            var node = _tail;
+            _tail = _tail.Previous;
+            if (_tail == null)
+                _head = null;
+            
+            return node.Value;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var current = _tail;
+            while (current != null && current.Value != null)
+            {
+                yield return current.Value;
+                current = current.Previous;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<T>)this).GetEnumerator();
+        }
+    }
+}
